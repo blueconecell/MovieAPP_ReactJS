cf919dc21e27d8802639a803d6e94564e12d4b3f
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-09-21 09:58:35 +0900

    update

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,8 +1,20 @@
-function App() {
-  return (
-    <div>
-      <h1>Welcome back!</h1>
-    </div>
-  );
+import React from "react";
+import Button from 'react-bootstrap/Button';
+
+function Button_(){
+    return (
+        <div>
+            <Button variant="primary">Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="success">Success</Button>
+            <Button variant="warning">Warning</Button>
+            <Button variant="danger">Danger</Button>
+            <Button variant="info">Info</Button>
+            <Button variant="light">Light</Button>
+            <Button variant="dark">Dark</Button>
+            <Button variant="link">Link</Button>
+        </div>
+    );
 }
-export default App;
+
+export default Button_;
